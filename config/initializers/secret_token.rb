be2fc0670ac7de1c7ab60210d86d5a4b8e8a1f9e
saasbook <saasbook@saasbook.(none)>
2012-10-10 11:59:55 -0700

    adding rails app files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitfulminds::Application.config.secret_token = '2c607256e3fe9d6a5ead7aebeb9bb4817771d0b32a968699fb017f404f864baa4ce570372f2dcaa21a3ba8ce7242bec8191a3336f53e44b3a5faebe3361ad4a3'
