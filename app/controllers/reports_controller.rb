ff7c36dc36ec4dd56cb37fa3552e7a30cf1814ce
saasbook <saasbook@saasbook.(none)>
2012-10-21 14:33:06 -0700

    Moved Call to Generate

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -8,8 +8,8 @@ class ReportsController < ApplicationController
     if school.presurveys.size > 0 and school.postsurveys.size > 0
       @report = Report.create!(:school_id => params[:school])
       flash[:notice] = "Report generated successfully for #{School.find(params[:school]).name}"
-      redirect_to portal_path
       #generate
+      redirect_to portal_path
     else
       flash[:warning] = "#{school.name} does not have pre/post surveys"
       redirect_to new_reports_path
